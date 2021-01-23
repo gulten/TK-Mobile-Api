22f1f9e78e52e46c0ba968131948c3d4cd6a363d
Gülten Deregözü <gulten.deregozu@gmail.com>
2021-01-23 23:00:05 +0300

    Add 'device table' migration file

---
+++ database/migrations/2021_01_23_152423_create_device_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDeviceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('device', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('uid');
+            $table->unsignedBigInteger('appId');
+            $table->string('language', 20);
+            $table->string('operating_system', 50);
+            $table->string('client_token')->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('device');
+    }
+}
